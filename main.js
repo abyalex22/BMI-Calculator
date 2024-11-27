758c9d5443b364b4fa39560100dfad5db354c1c9
abyalex22 <abyalex356@gmail.com>
2024-11-27 11:46:43 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+function calculate(){
+    // store user height
+    const height = (userHeight.value*0.01)
+    console.log(height);
+    
+    const weight = userWeight.value     
+        const bmi = (weight/(height)**2);
+        userBMI.value =`${bmi}kg/m^2 ` 
+}
